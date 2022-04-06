c57fe3105fb3f0f9f277677cf3ff07982e7b525f
duydiep08 <duydiep080498@gmail.com>
2022-04-06 18:21:15 +0700

    lan 1

---
+++ src/Phone.java
@@ -0,0 +1,7 @@
+public abstract class Phone {
+    abstract void insertPhone(String name, String phone);
+    abstract void removePhone(String name);
+    abstract void updatePhone(String name, String newPhone);
+    abstract void searchPhone(String name);
+    abstract void sort();
+}
