dba038f91aface8d29b0dfb3fc8c7c21997ee9db
duydiep08 <duydiep080498@gmail.com>
2022-04-07 19:01:03 +0700

    update BATAPJAVA 1

---
+++ src/NhanV.java
@@ -0,0 +1,12 @@
+public class NhanV {
+
+    public static void main(String[] args) {
+
+       /* NhanVien c = new NhanVien();
+        c.println();*/
+
+        NhanVien d = new NhanVien("Diep",23,150,2000,"Cau Giay", 0);
+        d.printInfo();
+
+    }
+}
